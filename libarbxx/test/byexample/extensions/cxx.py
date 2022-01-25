a6d04f625516801c98fd97282257ea66c474af92
Julian Rüth <julian.rueth@fsfe.org>
2022-01-25 00:16:44 -0500

    Fix include directories for byexample tests

--- libarbxx/test/byexample/extensions/cxx.py
+++ libarbxx/test/byexample/extensions/cxx.py
@@ -12,22 +12,22 @@ We also use cppyy instead of cling which takes care of the hard part of the
 parsing for us.
 """
 ######################################################################
-#  This file is part of exact-real.
+#  This file is part of arbxx.
 #
 #        Copyright (C) 2021 Julian Rüth
 #
-#  exact-real is free software: you can redistribute it and/or modify
+#  arbxx is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or (at your
 #  option) any later version.
 #
-#  exact-real is distributed in the hope that it will be useful,
+#  arbxx is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with exact-real. If not, see <https://www.gnu.org/licenses/>.
+#  along with arbxx. If not, see <https://www.gnu.org/licenses/>.
 #####################################################################
 from __future__ import unicode_literals
 import re
